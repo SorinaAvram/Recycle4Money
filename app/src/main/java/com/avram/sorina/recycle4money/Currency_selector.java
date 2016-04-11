9d364ba796e37a8e48605d3f3b61309f4c047d07
SorinaAvram <sorina.avram2000@yahoo.com>
2016-04-11 19:59:16 +0300

    New activities added

--- app/src/main/java/com/avram/sorina/recycle4money/Currency_selector.java
+++ app/src/main/java/com/avram/sorina/recycle4money/Currency_selector.java
@@ -1,29 +1,40 @@
 package com.avram.sorina.recycle4money;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Currency_selector extends AppCompatActivity {
+public class Currency_selector extends Activity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_selector);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.currency_selector, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
+    Intent startNewActivity = new Intent(this, Calculate.class);
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView) view;
+        Toast.makeText(this, "You Selected " + myText.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/avram/sorina/recycle4money/MainActivity.java
+++ app/src/main/java/com/avram/sorina/recycle4money/MainActivity.java
@@ -17,33 +17,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener{
 Spinner spinner;
 
 
        @Override
     protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
-           setContentView(R.layout.content_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
 
-           spinner= (Spinner)findViewById(R.id.spinner);
-           ArrayAdapter adapter= ArrayAdapter.createFromResource(this, R.array.recyclable_materials, android.R.layout.simple_spinner_item);
+        spinner= (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter adapter= ArrayAdapter.createFromResource(this, R.array.recyclable_materials, android.R.layout.simple_spinner_item);
            spinner.setAdapter(adapter);
+           spinner.getSelectedItem().toString();
            spinner.setOnItemSelectedListener(this);
        }
 
     public void NextMessage(View view) {
-        Intent startNewActivity = new Intent(this,QuantityActivity.class);
-        startActivity(startNewActivity);
+
+
     }
 
 
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+   public void onItemSelected(AdapterView<?> parent, View view, int position, long id)  {
+        String item=spinner.getSelectedItem().toString();
+        Intent startNewActivity = new Intent(this,QuantityActivity.class);
+        startNewActivity.putExtra("item", item);
+        startActivity(startNewActivity);
+
             TextView myText= (TextView) view;
             Toast.makeText(this,"You Selected "+myText.getText(),Toast.LENGTH_SHORT).show();
+
+
     }
 
     @Override
